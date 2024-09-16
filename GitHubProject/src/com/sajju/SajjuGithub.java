118d8ec132c6034d8def3564d937ce3374f3df88
sajjad khan <sajjadrockstar8294@gmail.com>
2024-09-16 15:39:38 +0530

    this project is commit

---
+++ GitHubProject/src/com/sajju/SajjuGithub.java
@@ -0,0 +1,10 @@
+package com.sajju;
+
+public class SajjuGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wel come to GitHub");
+	}
+
+}
